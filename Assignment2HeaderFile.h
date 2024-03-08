a08906c44bec6370d787b75d7f424439b37298ff
Snugpug00 <144748644+Snugpug00@users.noreply.github.com>
2024-03-07 22:59:00 -0600

    Add files via upload

---
+++ Assignment2HeaderFile.h
@@ -0,0 +1,16 @@
+// Sarah Miller
+// Assignment 2 Header File
+
+#include <iostream>
+using namespace std;
+
+void push_front(const Item_Type& item);
+void push_back(const Item_Type& item);
+void pop_front();
+void pop_back();
+void insert(size_t index, const Item_Type& item);
+bool remove(size_t index);
+size_t find(const Item_Type& item);
+int main() {
+    
+}
